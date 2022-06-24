b78890c6ba25b30e8b6072dfa61c576bf7c0f84f
David <David@DESKTOP-5A2HH1S>
2022-06-24 10:13:16 +0200

    commit con todos los ejercicios

---
+++ Condicionales/src/Ejercicio5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * 5. Algoritmo que determine si un año dado es bisiesto o no, teniendo en cuenta 
+ * que son años bisiestos los múltiplos de 4, excepto los que son también múltiplos 
+ * de 100 pero no lo son de 400.
+ * @author David
+ *
+ */
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce el año: ");
+		int año = entrada.nextInt();
+		
+		if ( (año%4) == 0 ) {
+			if ( (año%100 ==0) && (año%400 != 0)) {
+				System.out.println("El año NO es bisiesto");
+			} else {
+				System.out.println("El año es bisiesto");
+			}
+		} else {
+			System.out.println("El año NO es bisiesto");
+		}
+
+	}
+
+}
